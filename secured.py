e102570631132225e92c8fabc7722c899a499fd5
Gabe Appleton <gappleto97+development@gmail.com>
2016-04-22 23:02:50 -0700

    Create example implementation

---
+++ secured.py
@@ -0,0 +1,21 @@
+def secure_verify():
+	f = open(__file__, "rb")
+	text = f.read()
+	f.close()
+	from hashes import hashes, calls
+	return False not in (specific_hash(text, key) == hashes[key] for key in calls.keys())
+
+
+def get_hashes():
+	f = open(__file__, "rb")
+	text = f.read()
+	f.close()
+	from hashes import hashes, calls
+	return [specific_hash(text, key) for key in calls.keys()]
+
+
+def specific_hash(text, func):
+	import hashlib
+	m = hashlib.new(func)
+	m.update(text)
+	return m.hexdigest()
